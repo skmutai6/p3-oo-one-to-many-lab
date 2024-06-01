36874a570affc6dc0c79cb6b2b1316705febcccf
Shadrack <skmutai6@gmail.com>
2024-06-01 16:10:22 +0300

    lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner = 'Farmer' ):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Pet type not Valid")
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        pass
+
+    def add_pet(self, pet):
+        self.pet = pet
+        self.pet.append
+        
+
+    def get_sorted_pets(self):
+        pass
+
+    
+
+
